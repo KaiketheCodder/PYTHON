f9b6ceeb7eff3d5782ffd4b26f99e50021d70f88
KAIKE JULIANO DA SILVA <kaike.js@puccampinas.edu.br>
2024-08-26 11:20:17 -0300

    *

---
+++ Hello.py
@@ -0,0 +1,8 @@
+'''
+one = int (input("insira primeiro digito"))
+two = int (input("insira segundo digito"))
+print("a soma de ambos é ", one+two)
+
+'''
+one = int (input("insira um valor em metros"))
+print("Converter para milimetros", one*1000, "mm")
