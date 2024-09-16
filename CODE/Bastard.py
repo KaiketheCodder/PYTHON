6c73c4f2f26d75df5c6ade64c8dbc15c71da95a0
KaiketheCodder <Kaikejuliano22@gmail.com>
2024-09-16 11:14:50 -0300

    Add files via upload

---
+++ CODE/Bastard.py
@@ -0,0 +1,11 @@
+def funcional(): #Para criarmos uma função, usamos def (Nome da função)():
+    print("Hello World!")
+
+def jorge():
+    print("OH PORRA CARALHO")
+    funcional()#Uma função pode ser chamada de dentro de uma outra função
+print("HAHAHHAHHAA")
+funcional()#Para chamarmos uma função, apenas devemos copiar o nome dela com () e sem def.
+print("Sua CALCINHA caiu!")
+funcional()#temos a possibilidade de usar uma mesma função várias vezes, o quanto quisermos 
+jorge()
---
+++ CODE/Func.py
@@ -0,0 +1,8 @@
+def some(n1,n2):##As duas entradas entre os parenteses ditam quantos inputs possiveis podem ser utilizados pela variavel, entretanto, eles não são obrigatoriamente iguais os nomes das variaveis a serem usadas.
+    resultado = n1 + n2
+    print(resultado)
+
+num1 = int (input())
+num2 = int (input())
+some(num1, num2)##Aqui por exemplo, criamos as duas vars: Num1 e Num2, e usaremos essas duas para executar a função(Temos dois slots disponiveis para a função usar)
+
